9f512cfac1f1a7e6ddb2d0612663891066931c17
AdamS <surzyn.adam@gmail.com>
2024-09-10 21:43:01 +0200

    Dev: restoring non-build

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Roulette } from './components/roulette';
+
+function App() {
+  return (
+    <div className="App">
+      <Roulette></Roulette>
+    </div>
+  );
+}
+
+export default App;
